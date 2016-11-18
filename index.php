5843d5893fb636c908b095aa057c1c2c65e20ea9
Rama Patel <me@rexsdev.com>
2016-11-18 02:45:27 -0500

    Init
    Uploading the legal pages.

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="keywords" content="">
+		<meta name="author" content="Rexs development &amp; Design">
+		<title>Legal Pages</title>
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link href="legal.css" rel="stylesheet">
+	</head>
+<body>
+<div id="legal">
+	<div class="container">
+		<h1 class="page-title">Terms Of Service</h1>
+		<div class="section">
+			<h1>Section Title 1</h1>
+			<div class="sub-section">
+				<h2>Sub Heading 1</h2>
+				<p><b>1.1.1 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+				<p><b>1.1.2 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+			</div>
+			<div class="sub-section">
+				<h2>Sub Heading 2</h2>
+				<p><b>1.1.3 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+				<p><b>1.1.4 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+			</div>
+			<div class="sub-section">
+				<h2>Sub Heading 3</h2>
+				<p><b>1.1.5 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+				<p><b>1.1.6 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+			</div>
+		</div>
+		<hr>
+		<div class="section">
+			<h1>Section Title 2</h1>
+			<div class="sub-section">
+				<h2>Sub Heading 1</h2>
+				<p><b>2.0.1 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+				<p><b>2.1.2 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+			</div>
+			<div class="sub-section">
+				<h2>Sub Heading 2</h2>
+				<p><b>2.1.3 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+				<p><b>2.1.4 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+			</div>
+			<div class="sub-section">
+				<h2>Sub Heading 3</h2>
+				<p><b>2.1.5 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+				<p><b>2.1.6 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+			</div>
+		</div>
+			<hr>
+		<div class="section">
+		<h1>Section Title 3</h1>
+			<div class="sub-section">
+				<h2>Sub Heading 1</h2>
+				<p><b>3.1.1 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+				<p><b>3.1.2 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+			</div>
+			<div class="sub-section">
+				<h2>Sub Heading 2</h2>
+				<p><b>3.1.3 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+				<p><b>3.1.4 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+			</div>
+			<div class="sub-section">
+				<h2>Sub Heading 3</h2>
+				<p><b>3.1.5 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+				<p><b>3.1.6 -</b> Bacon ipsum dolor amet chuck frankfurter shank, leberkas salami picanha cupim ham. Turducken pork loin frankfurter fatback kevin ribeye meatloaf tail ham hock rump. Pork loin strip steak tail pork belly spare ribs pig. Andouille kielbasa picanha ground round beef ribs bresaola.</p>
+			</div>
+		</div>
+			<hr>
+		<small>Last date Modified: 10/20/2016</small>
+	</div>
+</div>
+</body>
+</html>
